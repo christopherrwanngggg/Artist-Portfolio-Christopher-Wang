82fbc0ad7cfa19650675bae8d0b6a9452f8bd60f
christopherrwanngggg <chrisw3235@g.ucla.edu>
2026-05-01 21:45:28 -0700

    initial commit

--- backend/firebase-config.ts
+++ backend/firebase-config.ts
@@ -5,13 +5,13 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyCoMQYWhg0GrfD8OG49QwxQOOMorYGMvS4",
-  authDomain: "artist-portfolio-9d6b8.firebaseapp.com",
-  projectId: "artist-portfolio-9d6b8",
-  storageBucket: "artist-portfolio-9d6b8.appspot.com",
-  messagingSenderId: "281662124647",
-  appId: "1:281662124647:web:b7e0912ca7f95aebcc1ac2",
-  measurementId: "G-5G7QPCK9E6",
+  apiKey: "AIzaSyCsNV_3vUeS6UQzVvDlk-n3PfJh5l3gX1U",
+  authDomain: "christopher-wang-design.firebaseapp.com",
+  projectId: "christopher-wang-design",
+  storageBucket: "christopher-wang-design.firebasestorage.app",
+  messagingSenderId: "846516669371",
+  appId: "1:846516669371:web:a0c978dda4535dd507529e",
+  measurementId: "G-VWBG14MNKJ"
 };
 
 var firebaseapp = null;
---
+++ backend/mock/mock.ts
@@ -0,0 +1,7 @@
+export const data = {
+  name: "Christopher Wang",
+  job: "Character Designer",
+  email: "doodleboy24047@gmail.com",
+  aboutme_para1: "First bio paragraph...",
+  aboutme_para2: "Second bio paragraph..."
+};
