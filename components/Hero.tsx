9561bb27e10a86370dc00b634b56e85479621f29
christopherrwanngggg <chrisw3235@g.ucla.edu>
2026-05-03 17:41:26 -0700

    “message”

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -48,7 +48,8 @@ export const Hero: React.SFC = () => {
         <div className="relative grid place-items-center order-1 md:order-2">
           <Image
             loader={modelImageLoader}
-            src="https://res.cloudinary.com/wecloud/image/upload/v1625592156/artist-portfolio/model_axxy6f.jpg"
+            src="pfp.jpeg"
+            //"https://res.cloudinary.com/wecloud/image/upload/v1625592156/artist-portfolio/model_axxy6f.jpg"
             alt="Picture of artist"
             objectFit="contain"
             height={imageDimensions[1]}
